2a946604daab43ff6bb17565c0b08d313397cfdd
Bagus Prasetyo <prasetyo.raharja12@gmail.com>
2019-11-29 23:55:30 +0800

    Sign in with google + migrate to android x

--- app/src/main/java/com/example/praktikumprogmob/GantiPassword.java
+++ app/src/main/java/com/example/praktikumprogmob/GantiPassword.java
@@ -2,15 +2,13 @@ package com.example.praktikumprogmob;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.praktikumprogmob.APIHelper.BaseApiService;
--- app/src/main/java/com/example/praktikumprogmob/Main2Activity.kt
+++ app/src/main/java/com/example/praktikumprogmob/Main2Activity.kt
@@ -1,12 +1,12 @@
 package com.example.praktikumprogmob
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v7.app.ActionBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.ActionBar
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.widget.TextView
 import com.example.praktikumprogmob.FragmentMenu.HomeFragment
@@ -15,7 +15,7 @@ import com.example.praktikumprogmob.FragmentMenu.ProfileFragment
 import com.example.praktikumprogmob.FragmentMenu.PromoFragment
 import android.content.DialogInterface
 import android.content.Intent
-import android.support.v7.app.AlertDialog
+import androidx.appcompat.app.AlertDialog
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
